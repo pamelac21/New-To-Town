5f93c8096c6e3b4e8f046710d38c61dc6d9be091
PamelaCleveland <pamelac021@gmail.com>
2021-09-16 14:35:39 -0400

    fixed output for 0 data

--- assets/script.js
+++ assets/script.js
@@ -1,87 +1,99 @@
+$(document).ready(function () {
+  function fetchData() {
+    //brew
+    fetch("https://api.openbrewerydb.org/breweries?by_postal=98826")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
 
-$(document).ready(function(){
-   function fetchData() {
-        //brew
-        fetch('https://api.openbrewerydb.org/breweries?by_postal=98826')
-            .then(response => response.json())
-            .then(data => console.log(data))
-        
-        //ticket
-        fetch('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=28146&apikey=SbkH1ltdeIub58BAAadKCyFaXfy7RKZa')
-            .then(response => response.json())
-            .then(data => console.log(data))
-        }
-        fetchData()
-        
-    //$("#myModal").modal('show'); ????
-    
-    $("#searchZip").on('click', myFunction())
-        
-     function myFunction() {
-        //variable to capture value of user input
-        let searchZip = document.getElementById('placeholder').value
-        
-            //store
-            localStorage.setItem('searchZip', searchZip)
-            console.log(localStorage)
-            
-            
-            
-            let queryUrl = ('https://api.openbrewerydb.org/breweries?by_postal=' + searchZip + '&sort=type,name:asc')
-            //fetch request using that input
-             fetch(queryUrl)
-                .then(response => response.json())
-            
-               
-                .then((response) => {
-                //variable selecting div where content will be displayed
-                //console.log(response)
+    //ticket
+    fetch(
+      "https://app.ticketmaster.com/discovery/v2/events.json?postalCode=28202&mode=no-cors&apikey=SbkH1ltdeIub58BAAadKCyFaXfy7RKZa"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  fetchData();
 
-            if (response.length === 0) {
-                console.log('nada')
-            } else {
-                console.log(response)
-                const rcBrew = document.querySelector("#response-container-brew")
-                rcBrew.innerHTML = ''
-                
-                for (let i = 0; i < 4; i++) {
+  //$("#myModal").modal('show'); ????
+  // $("#searchZip").on('click', myFunction())
+  const searchBtn = document.querySelector('#searchZip')
 
-                    $("#response-container-brew").append(`<ul><li>${response[i].name}</li>
+function myFunction(event) {
+  
+    //variable to capture value of user input
+    let searchZip = document.getElementById("placeholder").value;
+    event.preventDefault()
+    //store
+    localStorage.setItem("searchZip", searchZip);
+    console.log(localStorage);
+
+    let queryUrl =
+      "https://api.openbrewerydb.org/breweries?by_postal=" +
+      searchZip +
+      "&sort=type,name:asc";
+    //fetch request brews
+    fetch(queryUrl)
+      .then((response) => response.json())
+
+      .then((response) => {
+        //variable selecting div where content will be displayed
+        //console.log(response)
+
+        if (response.length === 0) {
+          $("#response-container-brew")
+              .append(`Sorry! None In Your Area`)
+          console.log("nada");
+        } else {
+          console.log(response);
+          const rcBrew = document.querySelector("#response-container-brew");
+          rcBrew.innerHTML = "";
+
+          for (let i = 0; i < 4; i++) {
+            $("#response-container-brew")
+              .append(`<ul><li>${response[i].name}</li>
                     <li>Phone: ${response[i].phone}</li>
                     <li>${response[i].street}</li>
-                    <li><a href="${response[i].website_url}">Brewery Website</a></li></ul>`)                   
-                }
-            }
-                console.log($('#response-container-brew'))
+                    <li><a href="${response[i].website_url}">Brewery Website</a></li></ul>`);
+          }
+        }
+        console.log($("#response-container-brew"));
 
-        
-            let ticketUrl = ('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=' + searchZip + '&mode=no-cors&apikey=SbkH1ltdeIub58BAAadKCyFaXfy7RKZa')
-            //fetch request using that input
-              fetch(ticketUrl)
-                .then(response => response.json())
-                .then((response) => {
-                console.log(response)
-                    let rcTicket = document.querySelector('#response-container-ticket')
-                    rcTicket.innerHTML = ''
-    
-                for (let i = 0; i < 4; i++) {
+        event.preventDefault()
+        let ticketUrl =
+          "https://app.ticketmaster.com/discovery/v2/events.json?postalCode=" +
+          searchZip +
+          "&mode=no-cors&apikey=SbkH1ltdeIub58BAAadKCyFaXfy7RKZa";
+        //fetch request ticketmaster
+        fetch(ticketUrl)
+          .then((response) => response.json())
+          .then((response) => {
+            if (!response._embedded) {
+              $("#response-container-ticket")
+                  .append(`Sorry! None In Your Area`)
+            console.log('nada');
+          } else {
+            let rcTicket = document.querySelector("#response-container-ticket");
+            rcTicket.innerHTML = "";
 
-                    let imgContent = document.createElement('img')
-                    imgContent.setAttribute('src', response._embedded.events[i].images[0].url)
-                    
-                    $("#response-container-ticket").append((imgContent),
-                    `<ul>    
+            for (let i = 0; i < 4; i++) {
+              let imgContent = document.createElement("img");
+              imgContent.setAttribute(
+                "src",
+                response._embedded.events[i].images[0].url
+              );
+
+              $("#response-container-ticket").append(
+                imgContent,
+                `<ul>    
                     <li>${response._embedded.events[i].name}</li>
                     <li><a href="${response._embedded.events[i].url}">Event Website</a></li>
-                    <li>${response._embedded.events[i].pleaseNote}</li></ul>`)
-                                     
-                }
+                    <li>${response._embedded.events[i].pleaseNote}</li></ul>`
+          );
+            }
 
-                console.log($('#response-container-ticket'))
-                
-                })
-
-        
-                })}
-            $("#searchZip").click(myFunction)
+            console.log($("#response-container-ticket"));
+          }});
+      });
+  }
+  $("#searchZip").click(myFunction);
 })
